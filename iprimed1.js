2d82ef8069f6dff587b735296604629d3fca3485
AntonV0 <anton.vlasenko94@gmail.com>
2026-02-24 14:03:27 +0000

    feat: add simple console and innerHTML message

---
+++ iprimed1.js
@@ -0,0 +1,13 @@
+// ! This is a simple JavaScript program that prints "Hello, World!" to the console.
+console.log('Hello, World!');
+
+// To run JavaScrip in Command prompt, use node filename.js
+
+// ? To connect JS file to HTML file, we can use the <script> tag in the HTML file. For example:
+// <script src="filename.js"></script>
+
+// To make text appear on the webpage, we can also use the innerHTML property. For example:
+document.body.innerHTML = "Hello, World!";
+
+// Another way (deprecated) to make text appear on the webpage is to use the document.write() method. For example:
+// document.write('Hello, World!');
